122b390b1ef6ddb4879a33f28b1bf50c3f669ef4
siddharth-I <67158060+siddharth-I@users.noreply.github.com>
2020-07-20 19:20:07 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("What is your name?");
+            name = Console.ReadLine();
+            int number;
+            Console.Write("Enter a whole number between 1 and 100: ");
+            number = int.Parse(Console.ReadLine());
+            if (number % 2 == 0 && number >= 2 & number <= 25)
+            {
+                Console.WriteLine("Even and less than 25");
+            }
+            else if (number % 2 == 0 && number >= 26 & number <= 60)
+            {
+                Console.WriteLine("Even");
+            }
+            else if (number % 2 == 0 && number > 60)
+            {
+                Console.WriteLine(number + " and even");
+            }
+            else if (number % 2 != 0)
+            {
+                Console.WriteLine(number + " and odd");
+            }
+            else if (number % 2 != 0 && number > 60)
+            {
+                Console.WriteLine(number + " and odd");
+            }
+        }
+    }
+}
